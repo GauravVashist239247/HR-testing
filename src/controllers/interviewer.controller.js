0d9f41a88c6aeeb367ea5224584019bd6ce60535
GauravVashist239247 <gvashist1010@gmail.com>
2026-02-25 12:22:08 +0530

    none

--- src/controllers/interviewer.controller.js
+++ src/controllers/interviewer.controller.js
@@ -63,7 +63,7 @@ export const loginInterviewer = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -9,8 +9,8 @@ export const generateToken = (res, interviewer) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: "none",
-    sameSite: "strict",
+    secure: false,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
