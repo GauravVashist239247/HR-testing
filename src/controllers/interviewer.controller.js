63cf3ac19df0c680d94dfe341cc30a541f6481f6
GauravVashist239247 <gvashist1010@gmail.com>
2026-02-25 14:15:24 +0530

    cookie debug

--- src/controllers/interviewer.controller.js
+++ src/controllers/interviewer.controller.js
@@ -60,23 +60,24 @@ export const loginInterviewer = async (req, res) => {
       expiresIn: "7d",
     });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Login successful",
-      data: {
-        id: interviewer._id,
-        name: interviewer.name,
-        email: interviewer.email,
-        role: interviewer.role,
-      },
-    });
+    res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true, // prevents JavaScript access
+        secure: true, // set to true if using HTTPS
+        sameSite: "none", // or 'Strict' or 'None' for cross-site
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+      })
+      .json({
+        success: true,
+        message: "Login successful",
+        data: {
+          id: interviewer._id,
+          name: interviewer.name,
+          email: interviewer.email,
+          role: interviewer.role,
+        },
+      });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
